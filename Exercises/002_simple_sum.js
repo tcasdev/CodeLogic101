ab4fb6c565d3fd56bf6720d0de1b447d578241fc
Thais Aguiar <227265729+tcasdev@users.noreply.github.com>
2026-02-26 12:48:30 -0300

    Create 002_simple_sum.js

---
+++ Exercises/002_simple_sum.js
@@ -0,0 +1,10 @@
+// Read two integers and print their sum
+
+const input = require("readline-sync");
+
+let n1 = Number(input.question("First integer: "));
+let n2 = Number(input.question("Second integer: "));
+
+sum = n1 + n2
+
+console.log(`SUM = ${sum}`);
